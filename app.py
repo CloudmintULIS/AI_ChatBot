514a7dc86cd9eb27fc2c1be27da5aed2961939f4
Cuong Le <dongongdz@gmail.com>
2025-07-06 22:14:00 +0700

    fix: move db.create_all outside main for Render

--- app.py
+++ app.py
@@ -42,7 +42,9 @@ def send():
 
     return jsonify({"reply": bot_reply})
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
+
